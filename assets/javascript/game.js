042c570dbf2701e1206f4718f7888240492491b1
Dara Cahill <daramcahill@icloud.com>
2020-04-17 12:42:54 -0400

    Add Upper and lowercase alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,7 +17,10 @@ function init (){
 }
 
 function pickRandomLetter(){
-    return 'A'; //TODO Comeback and do Math.Floor thing...
+    randomLetter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var a = randomLetter[pickRandomLetter]
+    return a;
+
 }
 
 document.onkeyup = function (event){
